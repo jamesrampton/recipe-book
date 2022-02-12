95f31405f1bc4fedf385dc872c52e6df8038fa91
James Rampton <me@jamesrampton.com>
2022-02-12 16:08:43 +0000

    Add ingredients by location tag

--- recipes/templatetags/recipe_tags.py
+++ recipes/templatetags/recipe_tags.py
@@ -4,8 +4,8 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag("tags/ingredients_tag.html")
-def recipe_ingredients(recipe):
+@register.inclusion_tag("tags/ingredients_by_team_tag.html")
+def recipe_ingredients_by_team(recipe):
     ingredients_data = OrderedDict()
     try:
         ingredients = recipe.ingredient_set.all().order_by('team', 'order')
@@ -14,5 +14,19 @@ def recipe_ingredients(recipe):
 
     for ingredient in ingredients:
         ingredients_data.setdefault(ingredient.team, []).append(ingredient)
+
+    return {'ingredients_data': ingredients_data}
+
+
+@register.inclusion_tag("tags/ingredients_by_location_tag.html")
+def recipe_ingredients_by_location(recipe):
+    ingredients_data = OrderedDict()
+    try:
+        ingredients = recipe.ingredient_set.all().order_by('location', 'order')
+    except AttributeError:
+        ingredients = []  # TODO check the type of recipe and raise a proper exception
+
+    for ingredient in ingredients:
+        ingredients_data.setdefault(ingredient.location, []).append(ingredient)
 
     return {'ingredients_data': ingredients_data}
