1f68f23d34b899abad71edebfa3d1fc9a7d2a4dd
James Rampton <me@jamesrampton.com>
2022-02-13 17:11:28 +0000

    Make views login required

--- recipe_book/urls.py
+++ recipe_book/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from recipes.views import RecipeListView, RecipeDetailView
+from django.contrib.auth.decorators import login_required
 
 admin.site.site_header = 'Recipe Book Admin'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RecipeListView.as_view()),
-    path('<slug:slug>', RecipeDetailView.as_view(), name="recipe_detail"),
+    path('', login_required, RecipeListView.as_view()),
+    path('<slug:slug>', login_required, RecipeDetailView.as_view(), name="recipe_detail"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
